ba6424e1b46f35567d02533e7bd84ce8ef8ed24c
Sami Hattab <hattab_sami@yahoo.fr>
2020-03-23 12:48:51 +0100

    prototype site parallax

---
+++ js/index2.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){		
+    $('#slide1').parallax("center", 0, 0.1, true);
+    $('#slide2').parallax("center", 900, 0.1, true);
+    $('#slide3').parallax("center", 2900, 0.1, true);
+})
