b2c74ec5afdc9ea2da8c6d9436b019c5f055cf17
Sami Hattab <hattab_sami@yahoo.fr>
2020-05-13 10:07:23 +0200

    the navbar now become transparent when the user start scrolling down

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(document).on("scroll", onScroll);
 
-  function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
+    function onScroll(event) {
+        var scrollPos = $(document).scrollTop();
+        $('a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else {
+                currLink.removeClass("active");
+            }
         });
+    }
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("nav").style.backgroundColor = '#055194';
+        } else {
+            document.getElementById("nav").style.backgroundColor = 'transparent';
+        }
+        prevScrollpos = currentScrollPos;
     }
 })
