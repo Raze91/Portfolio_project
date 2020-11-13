1912d8625c7675a55f6106517184c2709c728c17
Sami Hattab <hattab_sami@yahoo.fr>
2020-11-13 10:24:20 +0100

    bug fix, add the virtual keyboard project

---
+++ projects/Virtual_Keyboard/README.md
@@ -0,0 +1,12 @@
+# Virtual_Keyboard
+
+## English
+
+Virtual_Keyboard with HTML, CSS and Javascript.
+
+
+***
+
+## Français
+
+Clavier Virtuel avec de l'HTML, CSS et Javascript
---
+++ projects/Virtual_Keyboard/js/index.js
@@ -0,0 +1,75 @@
+window.addEventListener("load", function () {
+    const darkSwitch = document.querySelector("#darkSwitch");
+    const cssLink = document.querySelector("#link");
+    const buttons = document.querySelectorAll("input[type=button]");
+    const textOutput = document.querySelector("#text-output");
+    const capsLock = document.querySelector("#caps-lock");
+    const circle = document.querySelector(".circle");
+    let specials = document.querySelectorAll(".special")
+    let isCapsLock = false;
+
+    let majArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "°", "+", "¨", "£", "%", "µ", ">", "?", ".", "/", "§"];
+    let minArray = ["&", "é", "\"", "'", "(", "-", "è", "_", "ç", "à", ")", "=", "^", "$", "ù", "*", "<", ",", ";", ":", "!"];
+
+    // Dark Mode 
+    darkSwitch.addEventListener("click", function () {
+        if (this.value === "false") {
+            cssLink.href = "css/keyboard-dark.css";
+            this.value = "true"
+        } else {
+            cssLink.href = "css/keyboard-light.css";
+            this.value = "false";
+        }
+    });
+
+    // Caps Lock
+    capsLock.addEventListener("click", function () {
+        if (isCapsLock === false) {
+            circle.style.backgroundColor = "green";
+            isCapsLock = true;
+
+            document.querySelectorAll(".letter").forEach(button =>
+                button.value = button.value.toUpperCase()
+            )
+
+            for (let i = 0; i < specials.length; i++) {
+                specials[i].value = majArray[i];
+            }
+
+        } else {
+            circle.style.backgroundColor = "red";
+            isCapsLock = false;
+
+            document.querySelectorAll(".letter").forEach(button =>
+                button.value = button.value.toLowerCase()
+            )
+
+            for (let i = 0; i < specials.length; i++) {
+                specials[i].value = minArray[i];
+            }
+        }
+    });
+
+    // onclick
+    buttons.forEach(button =>
+        button.addEventListener("click", function () {
+            switch (this.value) {
+                case "SPACE":
+                    textOutput.value += " ";
+                    break;
+                case "Delete":
+                    textOutput.value = textOutput.value.substring(0, textOutput.value.length - 1);
+                    break;
+                case "Enter":
+                    textOutput.value += "\n";
+                    break;
+                case "Tab":
+                    textOutput.value += "   "
+                    break;
+                default:
+                    textOutput.value += this.value;
+            }
+        })
+    )
+
+})
